af6b6ce44c975a94f691c62de6ecfce465323e53
AlexMelnik2003 <alexz.melnik.02@gmail.com>
2024-08-06 17:18:37 +0600

    ok

--- logistic_site/settings.py
+++ logistic_site/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,8 +24,17 @@ SECRET_KEY = 'django-insecure-jt21+r-857p5le6+z+61ac)nv-5e$c4+5z4rkxzkt%a@l7lt5i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['albatrostrans.kg',
+                 'www.albatrostrans.kg',
+                 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://albatrostrans.kg',
+    'https://www.albatrostrans.kg',
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 # Application definition
 
@@ -70,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'logistic_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
